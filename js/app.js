372e61a27569ec323dc802f9fe9d8793c3ece61b
thewecas <thewecas@gmail.com>
2023-04-19 16:46:33 +0530

    pepsi detail modal with js added

--- js/app.js
+++ js/app.js
@@ -26,21 +26,38 @@ menu_checkbox.addEventListener("click", function () {
 // product page
 
 let cans = document.querySelectorAll(".can-container");
-let detail_modal = document.querySelector("#pepsi-detail");
-let modal_close = document.querySelector("#modal-close");
 
 cans.forEach(ele => {
     ele.addEventListener("click", function () {
-        openDetailModal(ele);
+        openDetailModal(ele.id);
+
     });
 });
 
-function openDetailModal(ele) {
+let detail_modal = document.querySelector("#pepsi-detail");
+
+let detail_title = detail_modal.children[1].children[0];
+let detail_image = detail_modal.children[2].children[0];
+
+let can_title, can_image;
+
+function openDetailModal(id) {
+    let ele = document.querySelector("#" + id);
+    can_title = ele.children[1].innerHTML;
+    can_image_link = ele.children[0].getAttribute("src");
+
+    detail_title.innerText = can_title;
+    detail_image.setAttribute("src", can_image_link);
+
     detail_modal.setAttribute("style", "display:flex;")
 }
+
+
+let modal_close = document.querySelector("#modal-close");
 
 modal_close.addEventListener("click", function () {
     detail_modal.setAttribute("style", "display:none;")
 
 });
+
 
